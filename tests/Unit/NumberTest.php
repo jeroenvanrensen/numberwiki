9e29851a6f2329a7d78dff09f48b783a7de434e6
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-07-08 13:15:46 -0400

    Add tests

---
+++ tests/Unit/NumberTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Number;
+
+it('can be instantiated', function (int $value) {
+    $number = new Number($value);
+    expect($number->number)->toBe($value);
+    expect((string) $number->number)->toBe((string) $value);
+})->with([0, 3, 8, 13, 209]);
+
+it('can compute its predecessor and successor', function (int $value, int|null $predecessor, int $successor) {
+    $number = new Number($value);
+    expect($number->predecessor()?->number)->toEqual($predecessor);
+    expect($number->successor()->number)->toEqual($successor);
+})->with([
+    [0, null, 1],
+    [3, 2, 4],
+    [8, 7, 9],
+    [13, 12, 14],
+    [209, 208, 210],
+]);
